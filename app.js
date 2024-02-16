35597ec3b6ad7aad54ff3dd82c44ba7f0f381f5b
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-02-16 21:26:19 +0500

    ok

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose")
-const swaggerJsDoc = require("swagger-jsdoc")
-const swaggerUi = require("swagger-ui-express")
-const SwaggerOptions = require("./swagger/swagger.json")
-const SwaggerDocs = swaggerJsDoc(SwaggerOptions)
-const dotenv = require("dotenv")
-dotenv.config()
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const SwaggerOptions = require("./swagger/swagger.json");
+const SwaggerDocs = swaggerJsDoc(SwaggerOptions);
+const dotenv = require("dotenv");
+dotenv.config();
+/**
+ * @swagger
+ * tags: 
+ *      -- name : admin
+ */
 
 mongoose.connect("mongodb+srv://ibrohimov:Abdulloh_070@clusteropera.jnxjp8x.mongodb.net/")
 .then(result => {
