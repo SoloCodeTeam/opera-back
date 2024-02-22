ff6cbd7b622b1d0aee7c23c7afaed4a692b348fd
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-02-22 22:58:28 +0500

    not end

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -22,19 +22,62 @@ exports.getProjectId = async(req,res,next) => {
     })
 }
 
-exports.getProject = async(req,res,next) => {
+exports.postProject = async(req,res,next) => {
     try {
         const {token} = req.headers
         await JWT.verify(token,process.env.JWT_KEY)
-        
+        const body = {
+            category: "project",
+            img: req.body.img,
+            title: req.body.title
+        }
+        return await Product.create(body).then(data => {
+            res.status(200).json(body)
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.satusCode =500}
+            next(err)
+        })
     } catch (err) {
-        
+        return res.status(404).json({message: "Not Allowed"})
     }
-    return await Product.find({category: "project"}).then(data => {
-        res.status(200).json(data)
-    }).catch(err => {
-        if (!err.statusCode) {
-            err.satusCode =500}
-        next(err)
-    })
+}
+
+exports.putProject = async(req,res,next) => {
+    try {
+        const {token} = req.headers
+        await JWT.verify(token,process.env.JWT_KEY)
+        id = req.params.projectId   
+        const body = {
+            category: "project",
+            img: req.body.img,
+            title: req.body.title
+        }
+        return await Product.findByIdAndUpdate(id,body).then(data => {
+            res.status(200).json({message: "Project Successfully updated"})
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.satusCode =500}
+            next(err)
+        })
+    } catch (err) {
+        return res.status(404).json({message: "Not Allowed"})
+    }
+}
+
+exports.deleteProject = async(req,res,next) => {
+    try {
+        const {token} = req.headers
+        await JWT.verify(token,process.env.JWT_KEY)
+        id = req.params.projectId
+        return await Product.findByIdAndDelete(id).then(data => {
+            res.status(200).json({message: "Project Successfully Deleted"})
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.satusCode =500}
+            next(err)
+        })
+    } catch (err) {
+        return res.status(404).json({message: "Not Allowed"})
+    }
 }
